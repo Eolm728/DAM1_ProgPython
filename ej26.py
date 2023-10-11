d7024d2eb819d066b5e9408c66282538271714e4
Eolm728 <eolm728@g.educaand.es>
2023-10-11 18:16:41 +0200

    Bit_changes

--- ej26.py
+++ ej26.py
@@ -1,4 +1,4 @@
-cesta_compra = input("Ingrese los productos de la cesta de compra separados por comas: ")
-productos = cesta_compra.split(',')
+compras = input("Ingrese los productos de la cesta de compra separados por comas: ")
+productos = compras.split(",")
 for producto in productos:
     print(producto.strip())
