e0c9d5d81d6609896d6cf1c9f2cbef051a24f0d0
Eolm728 <eolm728@g.educaand.es>
2023-10-23 16:34:22 +0200

    edited in my laptop

---
+++ P1.b2.py
@@ -0,0 +1,45 @@
+"""
+
+ancho = 10
+alto = 7
+
+resultado1 = ancho / alto
+resultado2 = ancho // 2
+resultado3 = ancho / 2
+resultado4 = ancho * 2
+resultado5 = ancho * alto
+resultado6 = (5 + 1) * 3
+resultado7 = (5 + 1) / 3
+
+print("ancho / alto: =", resultado1, "y es de tipo", type(resultado1),)
+print("ancho // 2 =", resultado2, "y es de tipo", type(resultado2),)
+print("ancho / 2 =", resultado3, "y es de tipo", type(resultado3),)
+print("ancho * 2 =", resultado4, "y es de tipo", type(resultado4),)
+print("ancho * alto =", resultado5, "y es de tipo", type(resultado5),)
+print("(5 + 1) * 3 =", resultado6, "y es de tipo", type(resultado6),)
+print("(5 + 1) / 3 =", resultado7, "y es de tipo" , type(resultado7),)
+
+
+
+farenheit_ºF = 0.0
+celsius_ºC = (farenheit_ºF- 32) * 5/9
+
+print("La temperatura en grados Farenheit es", farenheit_ºF,"", celsius_ºC,)
+
+
+
+num1 = 12.345
+num2 = 45.678
+num3 = 78.901
+num4 = 34.567
+num5 = 89.012
+num6 = 56.789
+
+print("Numero 1: ", num1,)
+print("Numero 2: ", num2,)
+print("Numero 3: ", num3,)
+print("Numero 4: ", num4,)
+print("Numero 5: ", num5,)
+print("Numero 6: ", num6,)
+
+"""
